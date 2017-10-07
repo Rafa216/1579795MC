c9c5a776832e861c5f8d4f297302bce0cc570022
Rafa216 <30887771+Rafa216@users.noreply.github.com>
2017-10-06 23:01:38 -0500

    Create pila.py

---
+++ pila.py
@@ -0,0 +1,25 @@
+class pila:
+  def __init__(self):
+    self.pila = []
+  def obtener(self):
+    return self.pila.pop()
+  def meter(self,e)
+    self.pila.append(e)
+    return len(self.pila)
+  @property
+  def longitud(self):
+    return len(self.pila)
+    
+p = pila()
+p.meter(1)
+p.meter(2)
+p.meter(3)
+p.meter(4)
+p.meter(5)
+p.meter(6)
+p.meter(100)
+p.meter(84583)
+
+print(p.longitud)
+print(p.obtener())
+print(p.obtener())
